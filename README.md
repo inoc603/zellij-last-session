0840fe567dc33a152cc3aefb781bf2f60ea53fcc
inoc603 <inoc603@gmail.com>
2025-03-02 11:15:04 +0800

    feat: implement switch to last session

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-## About
+## zellij-lats-session
 
-This is an example [Zellij][zellij] plugin in Rust. It can be used as a template to start developing your own plugins.
+A simple [zellij][zellij] plugin to quickly switch to last active session.
 
-More about Zellij plugins: [Zellij Documentation][docs]
+Load the plugin
 
-[zellij]: https://github.com/zellij-org/zellij
-[docs]: https://zellij.dev/documentation/plugins.html
+```kdl
+plugins {
+    switch-to-last-session location="file:/path/to/zellij-lats-session.wasm"
+}
 
-## Development
+load_plugins {
+    switch-to-last-session
+}
+```
 
-*Note*: you will need to have `wasm32-wasi` added to rust as a target to build the plugin. This can be done with `rustup target add wasm32-wasi`.
+Add key binding
 
-### With the Provided Layout
-
-![img-2024-11-14-100111](https://github.com/user-attachments/assets/e3bae15c-1f94-4d4a-acea-a036f8afdf67)
-
-
-Run `zellij -l zellij.kdl` at the root of this repository. This will open a development environment that will help you develop the plugin inside Zellij.
-
-It can also be used if you prefer developing outside of the terminal - in this case you should ignore the `$EDITOR` pane and use your IDE instead.
-
-### Otherwise
-
-1. Build the project: `cargo build`
-2. Load it inside a running Zellij session: `zellij action start-or-reload-plugin file:target/wasm32-wasi/debug/rust-plugin-example.wasm`
-3. Repeat on changes (perhaps with a `watchexec` or similar command to run on fs changes).
+```kdl
+    bind "r" {
+        MessagePlugin "switch-to-last-session" {
+            payload "switch-to-last-session"
+        };
+    SwitchToMode "Normal";
+    }
+```
--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,100 @@ use std::collections::BTreeMap;
 
 #[derive(Default)]
 struct State {
-    // the state of the plugin
+    sessions: BTreeMap<String, bool>,
+    current_session: Option<String>,
 }
 
 register_plugin!(State);
 
-// NOTE: you can start a development environment inside Zellij by running `zellij -l zellij.kdl` in
-// this plugin's folder
-//
-// More info on plugins: https://zellij.dev/documentation/plugins
+// TODO: maybe use the /cache directory?
+const SESSION_RECORD: &str = "/tmp/sessions.txt";
+
+// We store the most recent 2 session names in SESSION_RECORD:
+// <last_session>
+// <current_session>
+fn read_sessions() -> Vec<String> {
+    let res = std::fs::read_to_string(SESSION_RECORD);
+    if let Ok(content) = res {
+        content.lines().map(String::from).collect()
+    } else {
+        Vec::new()
+    }
+}
+
+// Write the 2 most recent session names to SESSION_RECORD.
+fn push_session_name(session_name: String) {
+    let mut sessions = read_sessions();
+
+    if sessions.is_empty() || sessions.last().map_or(true, |last| last != &session_name) {
+        sessions.push(session_name.clone());
+
+        if sessions.len() > 2 {
+            sessions = sessions.split_off(sessions.len() - 2);
+        }
+
+        if let Some(parent) = std::path::Path::new(SESSION_RECORD).parent() {
+            std::fs::create_dir_all(parent).expect("Failed to create directories");
+        }
+        let _ = std::fs::write(SESSION_RECORD, sessions.join("\n"));
+    }
+}
 
 impl ZellijPlugin for State {
-    fn load(&mut self, configuration: BTreeMap<String, String>) {
-        // runs once on plugin load, provides the configuration with which this plugin was loaded
-        // (if any)
-        //
-        // this is a good place to `subscribe` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.subscribe.html)
-        // to `Event`s (https://docs.rs/zellij-tile/latest/zellij_tile/prelude/enum.Event.html)
-        // and `request_permissions` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.request_permission.html)
+    fn load(&mut self, _configuration: BTreeMap<String, String>) {
+        request_permission(&[
+            PermissionType::ReadApplicationState,
+            PermissionType::ChangeApplicationState,
+        ]);
+        subscribe(&[EventType::PermissionRequestResult, EventType::SessionUpdate]);
     }
+
     fn update(&mut self, event: Event) -> bool {
-        let mut should_render = false;
-        // react to `Event`s that have been subscribed to (and the plugin has permissions for)
-        // return true if this plugin's `render` function should be called for the plugin to render
-        // itself
+        let should_render = false;
+
+        match event {
+            Event::SessionUpdate(info, _ignore) => {
+                let session_map: BTreeMap<String, bool> = info
+                    .iter()
+                    .map(|session| (session.name.clone(), session.is_current_session))
+                    .collect();
+
+                self.sessions = session_map.clone();
+
+                for (name, is_current_session) in &session_map {
+                    if !is_current_session {
+                        continue;
+                    }
+
+                    if self.current_session.is_none() || self.current_session != Some(name.clone())
+                    {
+                        push_session_name(name.clone());
+                    }
+
+                    self.current_session = Some(name.clone());
+
+                    break;
+                }
+            }
+            Event::Visible(_visible) => {}
+            _ => {}
+        }
+
         should_render
     }
-    fn pipe (&mut self, pipe_message: PipeMessage) -> bool {
-        let mut should_render = false;
-        // react to data piped to this plugin from the CLI, a keybinding or another plugin
-        // read more about pipes: https://zellij.dev/documentation/plugin-pipes
-        // return true if this plugin's `render` function should be called for the plugin to render
-        // itself
+    fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
+        let should_render = false;
+
+        if Some("switch-to-last-session") == pipe_message.payload.as_deref() {
+            let sessions = read_sessions();
+            if let Some(first_session) = sessions.first() {
+                if self.sessions.contains_key(first_session) {
+                    eprintln!("[switch] switch to session: {:?}", first_session);
+                    switch_session(Some(first_session));
+                }
+            }
+        }
+
         should_render
-    }
-    fn render(&mut self, rows: usize, cols: usize) {
-        println!("Hi there! I have {rows} rows and {cols} columns");
     }
 }
